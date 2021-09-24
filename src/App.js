9497eb9355d5e4be118a0043b9e4bdf272892791
Hyeongbok-Jeon <orbit0420@kakao.com>
2021-09-24 13:49:19 +0900

    한글

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    maxNo: 3,
+    boards: [
+      {
+        brdno: 1,
+        brdwriter: "Lee SunSin",
+        brdtitle: "If you intend to live then you die",
+        brddate: new Date(),
+      },
+      {
+        brdno: 2,
+        brdwriter: "So SiNo",
+        brdtitle: "Founder for two countries",
+        brddate: new Date(),
+      },
+    ],
+  };
+
+  handleSaveData = (data) => {
+    this.setState({
+      maxNo: this.state.maxNo + 1,
+      boards: this.state.boards.concat({
+        brdno: this.state.maxNo,
+        brddate: new Date(),
+        ...data,
+      }),
+    });
+  };
+
+  handleRemove = (brdno) => {
+    this.setState({
+      boards: this.state.boards.filter((row) => row.brdno !== brdno),
+    });
+  };
+
+  render() {
+    const { boards } = this.state;
+
+    return (
+      <div>
+        <BoardForm onSaveData={this.handleSaveData} />
+        <table border="1">
+          <tbody>
+            <tr align="center">
+              <td width="50">No.</td>
+              <td width="300">Title</td>
+              <td width="100">Name</td>
+              <td width="100">Date</td>
+            </tr>
+            {boards.map((row) => (
+              <BoardItem key={row.brdno} row={row} onRemove={this.handleRemove} onSelectRow={this.handleSelectRow} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class BoardItem extends Component {
+  handleRemove = () => {
+    const { row, onRemove } = this.props;
+    onRemove(row.brdno);
+  };
+
+  render() {
+    return (
+      <tr>
+        <td>{this.props.row.brdno}</td>
+        <td>
+          <a onClick={this.handleSelectRow}>{this.props.row.brdtitle}</a>
+        </td>
+        <td>{this.props.row.brdwriter}</td>
+        <td>{this.props.row.brddate.toLocaleDateString("ko-KR")}</td>
+        <td>
+          <button onClick={this.handleRemove}>X</button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+class BoardForm extends Component {
+  state = {};
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSaveData(this.state);
+    this.setState({});
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="title" name="brdtitle" onChange={this.handleChange} />
+        <input placeholder="name" name="brdwriter" onChange={this.handleChange} />
+        <button type="submit">Save</button>
+      </form>
+    );
+  }
 }
 
 export default App;
